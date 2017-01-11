f5eac168f712c790c0e925bde8b11a7869ce0e85
Krzysztof Joachimiak <kjoa.92@gmail.com>
2017-01-11 18:36:49 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,7 +1,93 @@
-from distutils.core import setup
+from distutils.core import setup, Extension
 from Cython.Build import cythonize
 import numpy as np
 
+# setup(
+# ext_modules = cythonize("sciquence/sequences/cy_searching.pyx", include_path = [np.get_include()])
+# )
+
+
+#!/usr/bin/env python
+
+import os
+import sys
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    from distutils.core import setup
+
+
+if sys.argv[-1] == 'publish':
+    os.system('python setup.py sdist upload')
+    sys.exit()
+
+# with open('sciquence/__init__.py') as fid:
+#     for line in fid:
+#         if line.startswith('__version__'):
+#             VERSION = line.strip().split()[-1][1:-1]
+#             break
+
+
+
+# Install requires
+
+with open('requirements.txt') as fid:
+    INSTALL_REQUIRES = [l.strip() for l in fid.readlines() if l]
+
+readme = open('README.rst').read()
+
+
+# Doclink
+doclink = """
+Documentation
+-------------
+The full documentation is at http://sciquence.rtfd.org."""
+#history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
+# Version
+VERSION = '0.1.1'
+
+
+# Cython
+
+ext = [
+    Extension("sciquence.sequences.cy_searching", ["sciquence/sequences/cy_searching.pyx"],
+              include_dirs=[np.get_include()]),
+    ]
+
+extensions = cythonize(ext)
+
+
+# Classifiers
+
+classifiers = [
+        'Programming Language :: Python',
+        'License :: OSI Approved :: MIT License',
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Topic :: Scientific/Engineering',
+    ]
+
+
 setup(
-ext_modules = cythonize("sciquence/sequences/cy_searching.pyx", include_path = [np.get_include()])
+    name='sciquence',
+    version=VERSION,
+    description='Time series & sequences processing in Python',
+    long_description=readme + '\n\n' + doclink + '\n\n',  #+ history,
+    author='Krzysztof Joachimiak',
+    # author_email='',
+    url='https://github.com/krzjoa/sciquence',
+    packages=find_packages(where='.', exclude=('tests')),
+    package_dir={'sciquence': 'sciquence'},
+    include_package_data=True,
+    install_requires=INSTALL_REQUIRES,
+    license='MIT',
+    zip_safe=False,
+    ext_modules=extensions,
+    keywords='sciquence',
+    classifiers=classifiers,
 )
