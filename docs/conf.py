1fb08b97f6f8f93e23c2ef274fece78250168cf0
Krzysztof Joachimiak <kjoa.92@gmail.com>
2017-01-17 20:28:59 +0100

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -21,7 +21,7 @@
 # sys.path.insert(0, os.path.abspath('.'))
 
 import os, sys
-sys.path.insert(0, os.path.abspath('.'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # -- General configuration ------------------------------------------------
 
