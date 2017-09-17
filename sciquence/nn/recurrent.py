a6dddd8662ef2db09f520087766f92768ba8801e
Krzysztof <joachimiak@mywhizzy.com>
2017-09-17 17:33:58 +0200

    Udemy RNN

--- sciquence/nn/recurrent.py
+++ sciquence/nn/recurrent.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # Krzysztof Joachimiak 2017
 # sciquence: Time series & sequences in Python
 #
@@ -6,9 +7,32 @@
 #
 # License: MIT
 
+import theano
+import theano.tensor as T
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from sklearn.utils import shuffle
+from neural_utils import init_weights, all_parity_pairs
 
 
 class RecurrentLayer(object):
 
-    def __init__(self, incoming, num_units):
+    def __init__(self, input_shape, num_units):
+
+        # Sizes
         self.num_units = num_units
+        self.input_shape = input_shape
+
+        D = input_shape[-1] # Liczba ficzerów
+        N = input_shape[0] # Liczba sekwencji wejściowych
+        M = num_units
+        K = 2 # Rozmiar wyjścia
+
+        # Trainable params
+        self.Wx = theano.shared(init_weights(D, M))                                        self.num_units))
+        self.Wh = theano.shared(init_weights(M, M))
+        self.bh = theano.shared(np.zeros(M))
+        self.h0 = theano.shared(np.zeros(M))
+        self.W0 = theano.shared(init_weights(M, K))
