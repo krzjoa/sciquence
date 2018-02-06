d4a5b3dcbb1bb275c0316ba9cfa3b584beb866c2
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2018-02-06 08:45:11 +0100

    Create binarizer.py

---
+++ sciquence/postprocessing/binarizer.py
@@ -0,0 +1,51 @@
+
+
+import numpy as np
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class ClasswiseBinarizer(BaseEstimator, TransformerMixin):
+    ''
+    Performing binarization classwise. 
+    
+    It may be used for binarize independtly multiple class in the tagging tasks.
+    
+    ''
+    def __init__(self, thresholds):
+        self.thresholds=thresholds
+        
+    def fit(self, X, y=None)
+        '' Does nothing ''
+        return self
+        
+    def transform(self, X, y=None, copy=False):
+       return (X >= self.thresholds).astype(float)
+       
+       
+
+class ClasswiseMeanBinarizer(BaseEstimator, TransformerMixin):
+    ''
+    Performing binarization classwise. 
+    
+    It may be used for binarize independtly multiple class in the tagging tasks.
+    
+    ''
+    def __init__(self):
+        self.thresholds=thresholds
+        
+    def fit(self, X, y=None)
+        '' Does nothing ''
+        return self
+        
+    def transform(self, X, y=None, copy=False):
+       return (X >= self.thresholds).astype(float)
+
+
+
+
+def binarize_classwise(X, thresholds):
+    ''
+    
+    Binarizization perforemed classwise.
+    
+    ''
+    return (X >= thresholds).astype(float)
