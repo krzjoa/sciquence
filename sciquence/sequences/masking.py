1327ca05e99236c4b9078e86ea2b8acc13b04f50
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2018-02-12 16:02:13 +0100

    Update masking.py

--- sciquence/sequences/masking.py
+++ sciquence/sequences/masking.py
@@ -24,3 +24,32 @@ def mask_sequences(list_of_seq):
     for i, l in enumerate(lens):
         mask[i, :l] = 1.
     return mask  
+
+
+def make_masked_input(list_of_seq):
+    '''
+
+    Make mask for sequences and coherent seqences block
+
+    Parameters
+    ----------
+    list_of_seq: list of Iterable
+        List of numpy array
+
+    Returns
+    -------
+    mask: numpy.ndarray
+        Mask
+    '''
+
+    lens = [len(seq) for seq in list_of_seq]
+    N, M = len(lens), max(lens)
+
+    mask = np.zeros((N, M))
+    input = np.zeros((N, M))
+
+    for i, l in enumerate(lens):
+        mask[i, :l] = 1.
+        input[i, :l] = list_of_seq[i]
+    return input, mask
+
