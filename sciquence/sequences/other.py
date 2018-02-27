cf48fdc67c38a481027ad55188b912e5740eca54
Krzysztof Joachimiak <joachimiak.krzysztof@gmail.com>
2018-02-27 10:08:46 +0100

    Create other.py

---
+++ sciquence/sequences/other.py
@@ -0,0 +1,14 @@
+
+
+# TODO: axis (?)
+def center_idx(array):
+    '''
+    Get cetral idx of iterable.
+    
+    Parameters
+    ----------
+    array: Iterable
+       An iterable object
+    
+    '''
+    return int(round(len(array)/2.))
